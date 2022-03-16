8fce4211d3d91facf430657bd84feb695764f267
Mihailo Rakic <90977529+rajkmajk@users.noreply.github.com>
2022-03-16 15:05:09 +0100

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -57,3 +57,24 @@
 # val1 -le val2 Returns true if val1 is less than or equal to val2
 ########
 
+#FILE CONDITIONS
+# FILE="test.txt"
+# if [ -f "$FILE" ]
+# then
+#     echo "$FILE exists"
+# else
+#     echo "$FILE does NOT exist"
+# fi
+
+
+########
+# -d file   True if the file is a directory
+# -e file   True if the file exists (note that this is not particularly portable, thus -f is generally used)
+# -f file   True if the provided string is a file
+# -g file   True if the group id is set on a file
+# -r file   True if the file is readable
+# -s file   True if the file has a non-zero size
+# -u    True if the user id is set on a file
+# -w    True if the file is writable
+# -x    True if the file is an executable
+########
