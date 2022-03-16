2a36f818894c2e24e8d9fbc652003493bb305b48
Mihailo Rakic <90977529+rajkmajk@users.noreply.github.com>
2022-03-16 15:14:54 +0100

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -78,3 +78,17 @@
 # -w    True if the file is writable
 # -x    True if the file is an executable
 ########
+
+#CASE STATEMENT (switch)
+# read -p "Are you 21 or over? Y/N " ANSWER
+# case "$ANSWER" in
+#     [yY] | [yY][eE][sS])
+#     echo "You can have a beer :)"
+#     ;;
+#     [nN] | [nN][oO])
+#     echo "Sorry, you cannot drink :("
+#     ;;
+# *)
+#     echo "Please enter y/yes or n/no"
+#     ;;
+#     esac
