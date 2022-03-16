f0881f404fc05a1043cc158ade439f8ae4abefaf
Mihailo Rakic <90977529+rajkmajk@users.noreply.github.com>
2022-03-16 15:02:04 +0100

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -39,3 +39,21 @@
 #     echo "Your name is not Mihailo or Pera"
 # fi
 
+#COMPARISON
+# NUM1=3
+# NUM2=5
+# if [ "$NUM1" -gt "$NUM2" ]
+# then
+#     echo "$NUM1 is greater than $NUM2"
+# else
+#     echo "$NUM1 is less than $NUM2"
+# fi
+########
+# val1 -eq val2 Returns true if the values are equal
+# val1 -ne val2 Returns true if the values are not equal
+# val1 -gt val2 Returns true if val1 is greater than val2
+# val1 -ge val2 Returns true if val1 is greater than or equal to val2
+# val1 -lt val2 Returns true if val1 is less than val2
+# val1 -le val2 Returns true if val1 is less than or equal to val2
+########
+
