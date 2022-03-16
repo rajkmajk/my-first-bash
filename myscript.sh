46c022e66ab2b13a91cba3fafb153c77f1062cae
Mihailo Rakic <90977529+rajkmajk@users.noreply.github.com>
2022-03-16 14:59:03 +0100

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -2,3 +2,12 @@
 
 # ECHO COMMAND
 # echo Hello World!
+
+# VARIABLES
+# Uppercase by convention
+# Letters, numbers, underscores
+#  NAME="Mihailo"
+# echo "My name is $NAME"
+# echo "My name is ${NAME}"
+
+
