00b52be2c74aab3406171722e57e61b21e049264
Mihailo Rakic <90977529+rajkmajk@users.noreply.github.com>
2022-03-17 18:12:34 +0100

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -109,3 +109,11 @@
 #      mv $FILE $NEW-$FILE
 #  done
 
+# WHILE LOOP - READ THROUGH A FILE LINE BY LINE
+# LINE=1
+# while read -r CURRENT_LINE
+#   do
+#     echo "$LINE: $CURRENT_LINE"
+#     ((LINE++))
+# done < "./new-1.txt"
+
